236a626abe7ef49e1be4c784b1d98b1b063819d7
8430177 <35485637+8430177@users.noreply.github.com>
2022-04-28 20:18:06 +0800

    Update postprocessing.py

--- postprocessing.py
+++ postprocessing.py
@@ -10,36 +10,31 @@ from datetime import datetime, timezone
 ghPagesURL = "https://8430177.github.io/TikTokRSS/"
 
 api = TikTokApi.get_instance()
-
 count = 10
-
-with open('subscriptions.csv') as f:
-    cf = csv.DictReader(f, fieldnames=['username'])
-    for row in cf:
-        user = row['username']
-
-        print (user)
-        print ("00000")
-
-        tiktoks = api.by_username(user, count=count)
-        
-        fg = FeedGenerator()
-        fg.id('https://www.tiktok.com/@' + user)
-        fg.title(user + ' TikTok')
-        fg.author( {'name':'Conor ONeill','email':'conor@conoroneill.com'} )
-        fg.link( href='http://tiktok.com', rel='alternate' )
-        fg.logo(ghPagesURL + '/tiktok-rss.png')
-        fg.subtitle('OK Boomer, all the latest TikToks from ' + user)
-        fg.link( href=ghPagesURL + 'rss/' + user + '.xml', rel='self' )
-        fg.language('en')
-
-        for tiktok in tiktoks:
-            fe = fg.add_entry()
-            link = "https://www.tiktok.com/@" + user + "/video/" + tiktok['id']
-            fe.id(link)
-            fe.published(datetime.fromtimestamp(tiktok['createTime'], timezone.utc))
-            fe.title(tiktok['desc'])
-            fe.link(href=link)
-            fe.description("<img src='" + tiktok['video']['cover'] + "' />")
-
-        fg.rss_file('rss/' + user + '.xml') # Write the RSS feed to a file
+with TikTokApi() as api:
+    with open('subscriptions.csv') as f:
+        cf = csv.DictReader(f, fieldnames=['username'])
+        for row in cf:
+            user = row['username']
+            print (user)
+            print ("00000")
+            tiktoks = api.user(username=user, count=count)
+            print (tiktoks)
+            fg = FeedGenerator()
+            fg.id('https://www.tiktok.com/@' + user)
+            fg.title(user + ' TikTok')
+            fg.author( {'name':'Conor ONeill','email':'conor@conoroneill.com'} )
+            fg.link( href='http://tiktok.com', rel='alternate' )
+            fg.logo(ghPagesURL + '/tiktok-rss.png')
+            fg.subtitle('OK Boomer, all the latest TikToks from ' + user)
+            fg.link( href=ghPagesURL + 'rss/' + user + '.xml', rel='self' )
+            fg.language('en')
+            for tiktok in tiktoks:
+                fe = fg.add_entry()
+                link = "https://www.tiktok.com/@" + user + "/video/" + tiktok['id']
+                fe.id(link)
+                fe.published(datetime.fromtimestamp(tiktok['createTime'], timezone.utc))
+                fe.title(tiktok['desc'])
+                fe.link(href=link)
+                fe.description("<img src='" + tiktok['video']['cover'] + "' />")
+            fg.rss_file('rss/' + user + '.xml') # Write the RSS feed to a file
